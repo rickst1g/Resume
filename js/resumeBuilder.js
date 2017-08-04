cdd2d16b8eeb5b15bb50756e5ae356b59b5e3d9a
Ricky Stephens <rstephens@rickystephens.com>
2017-08-03 20:07:24 -0500

    Add files via upload

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,116 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+//Work information
+var work = {
+	"jobs": [
+		{
+			"employer": "AT&T Entertainment Group",
+			"title": "Associate Applications Developer",
+			"location": "Richardson, TX, US",
+			"dates": "2016-Present",
+			"description": "Maintain and enhance the Visual Basic Application code in a massive MS Excel file"
+		},
+		{
+			"employer": "AT&T Internet Services",
+			"employerUrl": "http://att.jobs",
+			"title": "Uverse Single Point of Contact (USPOC)",
+			"location": "Richrdson, TX, US",
+			"dates": "2009-2016",
+			"description": "Identify and verify possible outages on the Uverse network. Then notify the approiate departments of the outages"
+		},
+		{
+			"employer": "AT&T Uverse Customer Service",
+			"employerUrl": "http://att.jobs",
+			"title": "Technical Support Representative",
+			"location": "Richardson, TX, US",
+			"dates": "2006-2009",
+			"description": "Accept Uverse customer calls and troubleshoot and resolve any issues"
+		}
+	]
+};
+
+//Projects information
+var projects = {
+	"projects": [
+		{
+			"title": "Redzone",
+			"date": "2016-2017",
+			"description": "MS Excel workbook used for access provisioning",
+			"images": ["images/tabularasa1.jpg","images/tabularasa2.jpg"]
+		},
+		{
+			"title": "Cable Failure",
+			"date": "2014",
+			"description": "Automated the cable failure process",
+			"images": ["images/cablefailuremacro.jpg"]
+		},
+		{
+			"title": "Access Provisioning",
+			"date": "2017",
+			"description": "Portal to send requests for tool access",
+			"images": ["images/tabularasa1.jpg","images/tabularasa2.jpg"]
+		},
+		{
+			"title": "Rick and Teri on vacation",
+			"date": "2013-2014",
+			"description": "Viking Long Boat trips down the Rhine and Danube rivers",
+			"images": ["images/trips/rhine/DSC00654.jpg","images/trips/danube/084.jpg"]
+		}
+	]
+};
+
+//Education information
+var education = {
+	"schools": [
+		{
+			"name": "Richland College",
+			"location": "Dallas,TX, US",
+			"degree": "Associate of Science",
+			"majors": "Biology",
+			"dates": "2014-2016",
+			"url": "http://www.richlandcollege.edu"
+		},
+		{
+			"name": "Stephen F Austin",
+			"location": "Nacogdoches,TX, US",
+			"degree": "Bachelor of Science",
+			"dates": "1972-1974",
+			"major": "Biology",
+			"url": "http://www.sfasu.edu"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Introduction to Programming",
+			"school": "Udacity",
+			"dates": "2017",
+			"url": "http://www.udacity.com"
+		},
+		{
+			"title": "Webpage Development",
+			"school": "Richland College",
+			"dates": "2017",
+			"url": "http://www.richlandcollege.edu"
+		},
+		{
+			"title": "MySQL Server",
+			"school": "Richland College",
+			"dates": "2017",
+			"url": "http://www.richlandcollege.edu"
+		}
+	]
+};
+
+//Personal information
+var bio = {
+ 	"name": "Ricky Stephens",
+ 	"role": "Associate Applications Developer",
+ 	"contacts": {
+ 		"mobile": "972-365-7425",
+ 		"email": "rstephens@rickystephens.com",
+ 		"github": "rickst1",
+ 		"location": "Richardson, TX, US"
+ 	},
+ 	"welcomeMessage": "Welcome to my Resume`",
+ 	"skills": ["Visual Basic Application", "MicroSoft Excel", "Management", "JavaScript", "Teaching", "Python", "HTML", "CSS"],
+ 	"bioPic": "images/me.jpg"
+ };
